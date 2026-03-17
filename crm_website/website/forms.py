e0609ea8ef24f8db96c4aa43e1c288bc101fa941
adhikariiii <prabitadhikari10@gmail.com>
2026-03-17 21:28:00 +0545

    updated form yet Updating the record is left

--- crm_website/website/forms.py
+++ crm_website/website/forms.py
@@ -1,16 +1,67 @@
 from django import forms
 from .models import Record
+
 class AddRecordForm(forms.ModelForm):
-    first_name = forms.CharField(required=True , widget= forms.widgets.TextInput(attrs={'placeholder': "First Name"}))
-    last_name =  forms.CharField(required=True , widget= forms.widgets.TextInput(attrs={'placeholder': "last Name"}))
-    city =   forms.CharField(required=True , widget= forms.widgets.TextInput(attrs={'placeholder': "city  "}))
-    address = forms.CharField(required=True , widget= forms.widgets.TextInput(attrs={'placeholder': "address"}))
-    email = forms.CharField(required=True , widget= forms.widgets.TextInput(attrs={'placeholder': "email"}))
-    phone = forms.CharField(required=True , widget= forms.widgets.TextInput(attrs={'placeholder': "phone"}))
-    state = forms.CharField(required=True , widget= forms.widgets.TextInput(attrs={'placeholder': "state"}))
-    zip_code = forms.CharField(required=True , widget= forms.widgets.TextInput(attrs={'placeholder': "zip code"}))
+    first_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+
+    city = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+
+    address = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+
+    email = forms.EmailField(   
+        required=True,
+        widget=forms.EmailInput(attrs={
+
+            'class': 'form-control'
+        })
+    )
+
+    phone = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={
+
+            'class': 'form-control'
+        })
+    )
+
+    state = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={
+
+            'class': 'form-control'
+        })
+    )
+
+    zip_code = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={
+
+            'class': 'form-control'
+        })
+    )
 
     class Meta:
         model = Record
-        exclude = ("user", )
-
+        exclude = ("user",)
